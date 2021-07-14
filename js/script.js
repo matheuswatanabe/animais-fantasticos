f261d4a9d0a5c68bb69d8bb00acb0bf4443e04a9
matheuswatanabe <matheuswatanabe1103@gmail.com>
2021-07-14 11:02:42 -0300

    primeiro commit

---
+++ js/script.js
@@ -0,0 +1,23 @@
+import initScrollsuave from './modules/scroll-suave.js';
+import initAnimacaoScroll from './modules/scroll-animacao.js';
+import initAccordion from './modules/accordion-list.js';
+import initTabNav from './modules/tabnav.js';
+import initModal from './modules/modal.js';
+import initTooltip from './modules/tooltip.js';
+import initDropdownMenu from './modules/dropdown-menu.js';
+import initMenuMobile from './modules/menu-mobile.js';
+import initFuncionamento from './modules/funcionamento.js';
+import initFetchAnimais from './modules/fetch-animais.js';
+import initFetchBitcoin from './modules/fetch-bitcoin.js';
+
+initTabNav();
+initAccordion();
+initAnimacaoScroll();
+initScrollsuave();
+initModal();
+initTooltip();
+initDropdownMenu();
+initMenuMobile();
+initFuncionamento();
+initFetchAnimais();
+initFetchBitcoin();
