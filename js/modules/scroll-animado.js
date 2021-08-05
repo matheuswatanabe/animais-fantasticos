749b96e549d8110041979bb8f7309460a0a6d095
matheuswatanabe <matheuswatanabe1103@gmail.com>
2021-08-05 17:50:18 -0300

    refatoracao com class do scroll animado

---
+++ js/modules/scroll-animado.js
@@ -0,0 +1,24 @@
+export default class ScrollAnimado {
+  constructor(sections) {
+    this.sections = document.querySelectorAll(sections);
+    this.startAnimation = window.innerHeight * 0.6;
+
+    this.animaScroll = this.animaScroll.bind(this);
+  }
+
+  animaScroll() {
+    this.sections.forEach((section) => {
+      const sectionTop = section.getBoundingClientRect().top - this.startAnimation;
+      if (sectionTop < 0) {
+        section.classList.add('ativo');
+      } else if (section.classList.contains('ativo')) {
+        section.classList.remove('ativo');
+      }
+    });
+  }
+
+  init() {
+    this.animaScroll();
+    window.addEventListener('scroll', this.animaScroll);
+  }
+}
