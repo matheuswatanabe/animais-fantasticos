056dfb2b4a299b000707d901ff095b4a821ef865
matheuswatanabe <matheuswatanabe1103@gmail.com>
2021-08-10 11:23:27 -0300

    dropdownmenu refatorado

--- js/script.js
+++ js/script.js
@@ -4,7 +4,7 @@ import Accordion from './modules/accordion-list.js';
 import TabNav from './modules/tabnav.js';
 import Modal from './modules/modal.js';
 import Tooltip from './modules/tooltip.js';
-import initDropdownMenu from './modules/dropdown-menu.js';
+import DropdownMenu from './modules/dropdown-menu.js';
 import initMenuMobile from './modules/menu-mobile.js';
 import initFuncionamento from './modules/funcionamento.js';
 import fetchAnimais from './modules/fetch-animais.js';
@@ -28,7 +28,9 @@ tooltip.init();
 const scrollAnimado = new ScrollAnimado('[data-anime="scroll"]');
 scrollAnimado.init();
 
-initDropdownMenu();
+const dropdownMenu = new DropdownMenu('[data-dropdown]');
+dropdownMenu.init();
+
 initMenuMobile();
 initFuncionamento();
 
